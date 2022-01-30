e6bdf579f33af8d20142450adcf65bf1ac261ce7
perplexedMurfy <perplexedMurfy@gmail.com>
2022-01-30 15:55:20 -0500

    Format in accordance with coding style

--- source/BluesBash.c
+++ source/BluesBash.c
@@ -7,74 +7,68 @@
 #pragma comment(lib, "user32.lib")
 #pragma comment(lib, "Shell32.lib")
 
-int main(void)
-{
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    const int screenWidth = 1280;
-    const int screenHeight = 720;
-    InitWindow(1280, 720, "title");
+int main(void) {
+	// Initialization
+	//--------------------------------------------------------------------------------------
+	const int screenWidth = 1280;
+	const int screenHeight = 720;
+	InitWindow(1280, 720, "title");
 
-    InitAudioDevice();
+	InitAudioDevice();
     
-    Sound C1 = LoadSound("resources/allNotes/C1.mp3");
-    Sound Eb1 = LoadSound("resources/allNotes/Eb1.mp3");
-    Sound G1 = LoadSound("resources/allNotes/G1.mp3");
+	Sound C1 = LoadSound("resources/allNotes/C1.mp3");
+	Sound Eb1 = LoadSound("resources/allNotes/Eb1.mp3");
+	Sound G1 = LoadSound("resources/allNotes/G1.mp3");
     
-    int placement = 0;
+	int placement = 0;
     
-    Sound keyboard[3] = {C1, Eb1, G1};
+	Sound keyboard[3] = {C1, Eb1, G1};
     
-    while(!WindowShouldClose()){
+	while(!WindowShouldClose()) {
         
-        if (IsKeyPressed(KEY_F)){
-            if((placement == 0) || (placement == 1)){
-                placement = placement + 1;
-            }
-            
-            PlaySound(keyboard[placement]);
-        
-        }
+		if (IsKeyPressed(KEY_F)) {
+			if((placement == 0) || (placement == 1)) {
+				placement = placement + 1;
+			}
+			PlaySound(keyboard[placement]);
+		}
         
         
-        if (IsKeyPressed(KEY_D)){
-            if((placement == 1) || (placement == 2)){
-                placement = placement - 1;
-            }
-            PlaySound(keyboard[placement]);
-        }
+		if (IsKeyPressed(KEY_D)) {
+			if((placement == 1) || (placement == 2)) {
+				placement = placement - 1;
+			}
+			PlaySound(keyboard[placement]);
+		}
         
-        if(IsKeyReleased(KEY_F)){
-            StopSound(keyboard[placement]);
-        }            
+		if(IsKeyReleased(KEY_F)) {
+			StopSound(keyboard[placement]);
+		}            
         
-        if(IsKeyReleased(KEY_D)){
-            StopSound(keyboard[placement]);
-        }
+		if(IsKeyReleased(KEY_D)) {
+			StopSound(keyboard[placement]);
+		}
         
-        if((IsKeyUp(KEY_F)) && (IsKeyUp(KEY_D))){
-            StopSound(keyboard[0]);
-            StopSound(keyboard[1]);
-            StopSound(keyboard[2]);
-        }
-        
-        BeginDrawing();
+		if((IsKeyUp(KEY_F)) && (IsKeyUp(KEY_D))) {
+			StopSound(keyboard[0]);
+			StopSound(keyboard[1]);
+			StopSound(keyboard[2]);
+		}
 
-            ClearBackground(RAYWHITE);
+		// Rendering
+		{
+			BeginDrawing();
+			ClearBackground(RAYWHITE);
+			EndDrawing();
+		}
+	}
 
-        EndDrawing();
-        
-    }
+	// De-Initialization
+	//--------------------------------------------------------------------------------------
+	CloseAudioDevice();         // Close audio device (music streaming is automatically stopped)
+	UnloadSound(C1);
+	CloseWindow();              // Close window and OpenGL context
+	//--------------------------------------------------------------------------------------
 
-
-   
-
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseAudioDevice();         // Close audio device (music streaming is automatically stopped)
-    UnloadSound(C1);
-    CloseWindow();              // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
-
-    return 0;
+	return 0;
 }
